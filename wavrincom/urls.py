6376cdcd34696132ae4de83e3f1b31c104de6005
Christopher Wavrin <wavrin@gmail.com>
2013-03-05 00:38:01 -0600

    fixed admin stylesss

--- wavrincom/urls.py
+++ wavrincom/urls.py
@@ -5,5 +5,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^blog/', include('blog.urls', namespace="post")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
