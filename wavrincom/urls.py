c98949e8ce61b2a93fb3ebf2a00ebf8a45aaa1f9
Christopher Wavrin <wavrin@gmail.com>
2013-03-05 00:34:40 -0600

    fixed admin styles

--- wavrincom/urls.py
+++ wavrincom/urls.py
@@ -6,3 +6,8 @@ urlpatterns = patterns('',
 	url(r'^blog/', include('blog.urls', namespace="post")),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
