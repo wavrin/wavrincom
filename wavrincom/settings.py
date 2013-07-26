e38c7a89962107a6535523bae2d2d7f2606d2a17
Christopher Wavrin <wavrin@gmail.com>
2013-07-25 23:33:46 -0500

    added google analytics

--- wavrincom/settings.py
+++ wavrincom/settings.py
@@ -141,10 +141,13 @@ INSTALLED_APPS = (
     'storages',
     'blog',
     'disqus',
+    'ganalytics',
 )
 
 DISQUS_API_KEY = '3SEISxDVpthkkvZJxNzpBBEnwtEbUYaDGumwCNLV2rCyRYTZqTFcsckCBaxPg7sC'
 DISQUS_WEBSITE_SHORTNAME = 'wavrin'
+
+GANALYTICS_TRACKING_CODE = 'UA-42536217-1'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
